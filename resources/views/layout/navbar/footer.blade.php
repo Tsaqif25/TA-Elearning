e225309919b3b724d8b94e01b3228b8f562687eb
Tsaqif25 <tsaqifluthfan7@gmail.com>
2025-11-15 09:20:47 +0700

    otw laravel filament untuk role wakur

--- resources/views/layout/navbar/footer.blade.php
+++ resources/views/layout/navbar/footer.blade.php
@@ -1,3 +1,19 @@
-  <footer class="text-center text-sm text-gray-500 py-8 border-t border-gray-200 bg-white">
-    © 2025 E-Learning SMK Negeri 2 Padang. All rights reserved.
-  </footer>
+<footer class="text-center text-sm text-gray-600 py-8 border-t border-gray-200 bg-white">
+    <div class="max-w-4xl mx-auto space-y-1">
+        <p class="font-semibold text-gray-700">© 2025 E-Learning SMK Negeri 2 Padang. All rights reserved.</p>
+
+        <p>
+            <span class="font-medium text-gray-700">Dosen Pembimbing:</span>
+            Dr. Titi Sriwahyuni, S.Pd., M.Eng
+        </p>
+
+        <p>
+            <span class="font-medium text-gray-700">Pengembang:</span>
+            Tsaqif Luthfan
+        </p>
+
+        <p>
+            <span class="font-medium text-gray-700">Universitas Negeri Padang</span>
+        </p>
+    </div>
+</footer>
